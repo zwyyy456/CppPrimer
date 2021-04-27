8fd0bf57badcbd4a8c07cd7a4c16c8a79e75bfd3
z wy <zwyyy456@zdeMacBook-Pro.lan>
2021-04-27 14:44:21 +0800

    macos

---
+++ chapter3/test3.31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = i;
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
